6d5035ba91d6248684e9dee4e83afc0fbaaeb6fa
Adrián <adriangalende@gmail.com>
2018-05-01 10:53:26 +0200

    test_matricula

--- src/main/java/org/formacio/component/ServeiAlumnat.java
+++ src/main/java/org/formacio/component/ServeiAlumnat.java
@@ -1,10 +1,13 @@
 package org.formacio.component;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ServeiAlumnat {
 
+	@Autowired
+	RepositoriAlumnes repositoriAlumnes;
 	
 	/**
 	 * ha de donar d'alta a la base de dades d'alumnes l'alumne indicat amb 
@@ -13,7 +16,11 @@ public class ServeiAlumnat {
 	 * Retorna true si l'alumne s'ha inserit, false si no.
 	 */
 	public boolean matricula (int id, String alumne) {
-		return true;
+		if (alumne != null) {
+			repositoriAlumnes.altaAlumne(id, alumne);
+			return true;
+		}
+		return false;
 	}
 	
 }
